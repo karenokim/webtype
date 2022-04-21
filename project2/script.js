06fac83b72664e722577d30ef8e0c1081c7a6c94
karenokim <gkim11@risd.edu>
2022-04-21 13:12:17 -0400

    warp and weft

--- project2/script.js
+++ project2/script.js
@@ -1,3 +1,12 @@
+
+function setup () {
+	noCanvas();
+	createInput ();
+}
+
+function draw (){
+
+}
 $(document).ready(function(){
 
 	console.log('script loaded');
@@ -86,31 +95,14 @@ $(document).ready(function(){
 });
 
 
-// Function to generate random number 
-function randomNumber(min, max) { 
-    return Math.floor(Math.random() * (max - min) + min);
+
+var input ;
+
+function setup () {
+	noCanvas();
+	input = createInput ();
 }
 
+function draw (){
 
-
-	// $('#intro').mousemove(function(event) {
-
-	// 	console.log(event.pageX, event.pageY);
-
-	// 	let cursorX = event.pageX / $(this).width();
-	// 	let cursorY = 1 - (event.pageY) / $(this).height();
-
-	// 	let settingX = Math.floor(cursorX * wdthmax);
-	// 	let settingY = Math.floor(cursorY * wghtmax);
-
-	// 	// console.log( settingX, settingY)
-
-	// 	$("#plain").css({
-	// 		"--wdth": settingX,
-	// 		"--wght": settingY
-	// 	});
-		
-	// });
-
-
-
+}
