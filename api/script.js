98240768514d3ad355ff3c9673925645a342c518
karenokim <gkim11@risd.edu>
2022-05-08 13:30:30 -0400

    ready

--- api/script.js
+++ api/script.js
@@ -7,19 +7,15 @@ $(document).ready(function(){
 
          let word = item.word;
          console.log(word);
-
          $('#word').text(word);
 
          let sound = item.pronounciation;
          console.log(sound);
-
          $('#pronounciation').text(sound);
-         // $('.salad').text( saladname );
+        
+        let meaning = item.meaning;
+        console.log(meaning);
 
-         // for(let i = 0; i<data.length; i++ ){
-         //   console.log(i, data[i]);
-         //   $('.word').append('<div class="word">' + data[i] +'</div>')
-         // }
       });
 
 }); 
