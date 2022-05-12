0e86526839d33b466257ebefb9c1819585f29359
karenokim <gkim11@risd.edu>
2022-05-11 23:31:08 -0400

    0102

--- api/script.js
+++ api/script.js
@@ -1,23 +1,54 @@
+let words;
+let counter = 0;
+
 $(document).ready(function(){   
 
   $.getJSON("words.json")
       .done(function( data ) {
          console.log(data);
-         let item  = data[0];
 
-         let word = item.word;
-         console.log(word);
-         $('#word').text(word);
+         words = data;
 
-         let sound = item.pronounciation;
-         console.log(sound);
-         $('#pronounciation').text(sound);
-        
-        let meaning = item.meaning;
-        console.log(meaning);
-        $('#meaning').text(meaning);
+          showWord(counter);
+
+         $('.col2').click(function(){
+
+          if(counter < 12){        
+            counter ++; // increment fact index until 5
+          }else{
+            counter = 0; // reset fact index
+          }
+          console.log(counter);
+          showWord(counter);
+       
+        });
 
       });
 
 }); 
+
+
+function showWord(index){
+
+   let item  = words[index];
+
+   let word = item.word;
+   // console.log(word);
+   $('#word').text(word);
+
+   let sound = item.pronounciation;
+   // console.log(sound);
+   $('#pronounciation').text(sound);
   
+  let meaning = item.meaning;
+  // console.log(meaning);
+  $('#meaning').text(meaning);
+
+  let image = item.image;
+  $('#image img').attr('src', 'asset/'+image);
+
+  let background = item.bg;
+  $('.col2').css('background',background);
+
+
+}
